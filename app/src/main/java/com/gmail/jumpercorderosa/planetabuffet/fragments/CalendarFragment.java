a68e4a95df20a4436598ac352436662b297148c2
renanbrando <reebrando@gmail.com>
2017-09-10 09:09:02 -0300

    Adicionado fragment com data

--- app/src/main/java/com/gmail/jumpercorderosa/planetabuffet/fragments/CalendarFragment.java
+++ app/src/main/java/com/gmail/jumpercorderosa/planetabuffet/fragments/CalendarFragment.java
@@ -1,41 +1,40 @@
 package com.gmail.jumpercorderosa.planetabuffet.fragments;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gmail.jumpercorderosa.planetabuffet.R;
 import com.gmail.jumpercorderosa.planetabuffet.db.DBHandler;
 import com.gmail.jumpercorderosa.planetabuffet.model.User;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import static com.gmail.jumpercorderosa.planetabuffet.activity.MainActivity.trocaFragmento;
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by danielle on 20/08/2017.
  */
 
-public class CalendarFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+public class CalendarFragment extends Fragment {
 
     private DBHandler db;
+    DatePicker simpleDatePicker;
+    Button submit;
     private static final String PREFS_NAME = "pref";
+    int year;
+    int month;
+    int day;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
         final View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-
+        simpleDatePicker = (DatePicker) view.findViewById(R.id.simpleDatePicker);
 
         setHasOptionsMenu(true);
         db = new DBHandler(getContext());
@@ -47,92 +46,24 @@ public class CalendarFragment extends Fragment implements DatePickerDialog.OnDat
         int aux = db.getUsersCount();
         final User user = db.getUser(user_id);
 
+        submit = (Button) view.findViewById(R.id.picDate);
+        // perform click event on submit button
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the values for day of month , month and year from a date picker
+                String day = "Day = " + simpleDatePicker.getDayOfMonth();
+                String month = "Month = " + (simpleDatePicker.getMonth() + 1);
+                String year = "Year = " + simpleDatePicker.getYear();
+                // display the values by using a toast
+                Toast.makeText(getApplicationContext(), day + "\n" + month + "\n" + year, Toast.LENGTH_LONG).show();
+            }
+        });
 
-        DatePickerFragment datePicker = new DatePickerFragment();
-        datePicker.onCreateDialog(savedInstanceState);
 
         //trocaFragmento(R.id.main_fragment, new CountdowmnFragment());
         return view;
     }
 
-
-    public static class DatePickerFragment extends DialogFragment {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            return new DatePickerDialog(getContext(),
-                    (DatePickerDialog.OnDateSetListener)
-                            getContext(), year, month, day);
-        }
-
-    }
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        Calendar cal = new GregorianCalendar(year, month, day);
-        setDate(cal);
-    }
-
-    private void setDate(final Calendar calendar) {
-        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        ((TextView) getView().findViewById(R.id.showDate)).setText(dateFormat.format(calendar.getTime()));
-    }
 }
-
-/*
-public class static extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-
-
-    @Override
-
-
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        //obtem dados do usuario para atualiza-lo
-
-        SharedPreferences sharedPref = getContext().getSharedPreferences(PREFS_NAME, getContext().MODE_PRIVATE);
-        int user_id = sharedPref.getInt("user_id", 0);
-
-        int aux = db.getUsersCount();
-        final User user = db.getUser(user_id);
-
-
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
-        dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
-
-        return new DatePickerDialog(getActivity(),
-                (DatePickerDialog.OnDateSetListener)
-                        getActivity(), year, month, day);
-
-        //o que fazer com esse hardcode???
-
-
-        String format_date = Integer.toString(day) + '/' + Integer.toString(month) + '/' + Integer.toString(year);
-
-        user.setEventDate(format_date);
-        db.updateUser(user);
-
-        trocaFragmento(R.id.main_fragment, new CountdownFragment());
-
-        return dialog;
-
-    }
-
-    //???
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        //btnDate.setText(ConverterDate.ConvertDate(day, month + 1, year));
-    }
-
-}
-*/
-
 
